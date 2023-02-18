ec62af05c2ed0bc11d6400e085d14497796d066c
joris <97834949+Georgyporgie@users.noreply.github.com>
2023-02-18 17:36:36 +0100

    Add files via upload

---
+++ node/server.js
@@ -0,0 +1,20 @@
+var http = require('http'),
+    url = require('url'),
+    fs   = require('fs'),
+    filePath = Sunny hot radio/muziek',
+    stat = fs.statSync(filePath);
+
+http.createServer(function(request, response) {
+    var queryData = url.parse(request.url, true).query;
+    const skip = typeof(queryData.skip) == 'undefined' ? 0 : queryData.skip;
+    const startByte = stat.size * skip;
+
+    response.writeHead(200, {
+        'Content-Type': 'audio/mpeg',
+        'Content-Length': stat.size - startByte
+    });
+
+    // We replaced all the event handlers with a simple call to util.pump()
+    fs.createReadStream(filePath, {start:startByte}).pipe(response);
+})
+.listen(3000);
