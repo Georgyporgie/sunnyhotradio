67ee7d165568a376c4b0c84b4d316b9cba40b6c3
joris <97834949+Georgyporgie@users.noreply.github.com>
2023-10-27 19:29:38 +0200

    Add files via upload

--- node/main2.js
+++ node/main2.js
@@ -206,7 +206,7 @@ let track_list = [
      name: "Welcome!",
     artist: "Listen Live Sunny Danceradio",
     image: "https://i.ibb.co/z6h40FW/saturday-night-fever-1977.png",
-    path: "muziek/jingles/let's go back.mp3",
+    path: "muziek/jingles/back in time jingle.mp3",
   },
 
 
