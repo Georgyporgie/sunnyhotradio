51656c81238954109b6f5c6649031b50682f3a1f
joris <97834949+Georgyporgie@users.noreply.github.com>
2024-03-26 18:06:53 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -15,3 +15,37 @@ function myFunction1() {
  $('.btn').click(function () {
             $(this).toggleClass("click");
         });
+
+
+
+
+
+
+
+
+
+function update() {
+ var el = document.querySelector(
+  ".content, .more");
+ el.innerHTML =
+  "A beautiful mix of new Dance songs<br> and older Dance & Disco classics!";
+};
+
+
+
+function update2() {
+ var el = document.querySelector(
+  ".inhoud, .meer");
+ el.innerHTML =
+  "A beautiful mix of new Rock & Pop songs<br> to help you through the day!";
+};
+
+
+
+
+function update3() {
+ var el = document.querySelector(
+  ".jazzex, .minder");
+ el.innerHTML =
+  "A beautiful mix of classicjazz & Lounge songs<br> to help you through the day!";
+};
