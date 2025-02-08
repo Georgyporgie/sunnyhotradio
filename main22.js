705b31909f276857e09958709f979263caf9634a
joris <97834949+Georgyporgie@users.noreply.github.com>
2025-02-08 17:42:23 +0100

    Add files via upload

--- main22.js
+++ main22.js
@@ -358,7 +358,12 @@ let track_list = [
 
 
 
-
+{
+    name: "  Welcome!",
+    artist: "Listen Live Sunny Danceradio  ",
+    image: "https://i.ibb.co/z6h40FW/saturday-night-fever-1977.png",
+    path: "muziek/jingles/New track.mp3",
+  },
 
 
 
