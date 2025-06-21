779a8505c1c2ff804ef82e414a7e88523390c456
joris <97834949+Georgyporgie@users.noreply.github.com>
2025-06-22 00:40:10 +0200

    Update main24.js

--- main24.js
+++ main24.js
@@ -121,7 +121,7 @@ function getCurrentTimeCategory() {
       } else if (currentHour >= 17 && currentHour < 21) {
         category = "f evening-late";
       } else if ((currentHour >= 21 && currentHour <= 23) || (currentHour >= 0 && currentHour < 3)) {
-        category = "f evening";
+        category = "f afternoon";
       } else {
         category = "jingle-time";
       }
