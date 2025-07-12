4527352a0fa0f02b5a66f75caff03e7bfe26d72f
joris <97834949+Georgyporgie@users.noreply.github.com>
2025-07-12 19:40:43 +0200

    Add files via upload

--- main24.js
+++ main24.js
@@ -20249,7 +20249,7 @@ timeCategory: "f evening"
 
 {
        name: "-------------------------",
-    artist: "----------------------------------------------",
+    artist: "",
     image: "https://i.ibb.co/z6h40FW/saturday-night-fever-1977.png",
     path: "muziek/jingles/Sunny radio jingle3.mp3",
     timeCategory: "f evening-late"
@@ -20339,8 +20339,8 @@ timeCategory: "f evening"
 
 
 {
-           name: "-------------------------",
-    artist: "----------------------------------------------",
+       name: "-------------------------",
+    artist: "",
     image: "https://i.ibb.co/z6h40FW/saturday-night-fever-1977.png",
     path: "muziek/jingles/Sunny radio jingle3.mp3",
  timeCategory: "f evening-late"
@@ -20375,8 +20375,8 @@ timeCategory: "f evening"
 
 
 {
-            name: "-------------------------",
-    artist: "----------------------------------------------",
+         name: "-------------------------",
+    artist: "",
     image: "https://i.ibb.co/z6h40FW/saturday-night-fever-1977.png",
     path: "muziek/jingles/Sunny radio jingle3.mp3",
  timeCategory: "f evening-late"
@@ -20474,8 +20474,8 @@ timeCategory: "f evening"
 
 
 {
-           name: "-------------------------",
-    artist: "----------------------------------------------",
+             name: "-------------------------",
+    artist: "",
     image: "https://i.ibb.co/z6h40FW/saturday-night-fever-1977.png",
     path: "muziek/jingles/Sunny radio snippet.mp3",
  timeCategory: "f evening-late"
@@ -20502,8 +20502,8 @@ timeCategory: "f evening"
  timeCategory: "f evening-late"
 },
 {
-          name: "-------------------------",
-    artist: "----------------------------------------------",
+             name: "-------------------------",
+    artist: "",
     image: "https://i.ibb.co/z6h40FW/saturday-night-fever-1977.png",
     path: "muziek/jingles/Sunny radio jingle3.mp3",
  timeCategory: "f evening-late"
@@ -20547,8 +20547,8 @@ timeCategory: "f evening"
 
 
 {
-           name: "-------------------------",
-    artist: "----------------------------------------------",
+             name: "-------------------------",
+    artist: "",
     image: "https://i.ibb.co/z6h40FW/saturday-night-fever-1977.png",
     path: "muziek/jingles/do not turnoff receiver.mp3",
   timeCategory: "f evening-late"
@@ -20623,8 +20623,8 @@ timeCategory: "f evening"
 },
 
 {
-            name: "-------------------------",
-    artist: "----------------------------------------------",
+               name: "-------------------------",
+    artist: "",
     image: "https://i.ibb.co/z6h40FW/saturday-night-fever-1977.png",
     path: "muziek/jingles/Sunny radio jingle1.mp3",
  timeCategory: "f evening-late"
@@ -20688,8 +20688,8 @@ timeCategory: "f evening"
 
 
 {
-          name: "-------------------------",
-    artist: "----------------------------------------------",
+           name: "-------------------------",
+    artist: "",
     image: "https://i.ibb.co/z6h40FW/saturday-night-fever-1977.png",
     path: "muziek/jingles/Let op.mp3",
  timeCategory: "f evening-late"
@@ -20702,16 +20702,16 @@ timeCategory: "f evening"
 
 
 {
-          name: "-------------------------",
-    artist: "----------------------------------------------",
+             name: "-------------------------",
+    artist: "",
     image: "https://i.ibb.co/z6h40FW/saturday-night-fever-1977.png",
     path: "muziek/jingles/nos journaal 11.mp3",
    timeCategory: "f evening-late"
 },
 
 {
-           name: "-------------------------",
-    artist: "----------------------------------------------",
+             name: "-------------------------",
+    artist: "",
     image: "https://i.ibb.co/z6h40FW/saturday-night-fever-1977.png",
     path: "muziek/jingles/sunny dance jingle.mp3",
  timeCategory: "f evening-late"
@@ -20777,8 +20777,8 @@ timeCategory: "f evening"
 
 
 {
-           name: "-------------------------",
-    artist: "----------------------------------------------",
+              name: "-------------------------",
+    artist: "",
     image: "https://i.ibb.co/z6h40FW/saturday-night-fever-1977.png",
     path: "muziek/discjockeys/40 in jingle22.mp3",
   timeCategory: "f evening-late"
@@ -20807,8 +20807,8 @@ timeCategory: "f evening"
 
 
 {
-            name: "-------------------------",
-    artist: "----------------------------------------------",
+              name: "-------------------------",
+    artist: "",
     image: "https://i.ibb.co/z6h40FW/saturday-night-fever-1977.png",
     path: "muziek/jingles/sunny dance jingle.mp3",
    timeCategory: "f evening-late"
@@ -20870,8 +20870,8 @@ timeCategory: "f evening"
 
  
 {
-           name: "-------------------------",
-    artist: "----------------------------------------------",
+              name: "-------------------------",
+    artist: "",
     image: "https://i.ibb.co/z6h40FW/saturday-night-fever-1977.png",
     path: "muziek/discjockeys/40 in jingle11.mp3",
  timeCategory: "f evening-late"
@@ -20885,8 +20885,8 @@ timeCategory: "f evening"
 
 
 {
-           name: "-------------------------",
-    artist: "----------------------------------------------",
+              name: "-------------------------",
+    artist: "",
     image: "https://i.ibb.co/z6h40FW/saturday-night-fever-1977.png",
     path: "muziek/discjockeys/40 in jingle22.mp3",
  timeCategory: "f evening-late"
