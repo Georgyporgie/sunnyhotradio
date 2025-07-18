bc15415c1ae2a3b632356d809284c3052376ad1e
joris <97834949+Georgyporgie@users.noreply.github.com>
2025-07-18 21:07:41 +0200

    Add files via upload

--- voiceover.js
+++ voiceover.js
@@ -1,13 +1,6 @@
+// voiceover.js
 export async function generateScript(track, timeCategory) {
   // Just return a pretend script for now
   return `Now playing: ${track}, perfect for your ${timeCategory} vibes.`;
 }
 
-// main.js
-
-import { setupRadio, playTrack } from './webradio.js';
-
-document.addEventListener("DOMContentLoaded", () => {
-  setupRadio();
-  playTrack();
-});
