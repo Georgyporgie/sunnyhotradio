5f1c5f043ac57e8d070996ad37c2bb932c0939e8
joris <97834949+Georgyporgie@users.noreply.github.com>
2025-12-15 17:44:07 +0100

    Delete voiceover.js

--- voiceover.js
+++
@@ -1,6 +0,0 @@
-// voiceover.js
-export async function generateScript(track, timeCategory) {
-  // Just return a pretend script for now
-  return `Now playing: ${track}, perfect for your ${timeCategory} vibes.`;
-}
-
